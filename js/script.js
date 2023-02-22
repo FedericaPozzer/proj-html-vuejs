bef14d32a55ea097feff95239483d454d6584489
Federica Pozzer <federica.pozzer@gmail.com>
2023-02-22 17:02:25 +0100

    project done!

--- js/script.js
+++ js/script.js
@@ -5,49 +5,49 @@ createApp({
         return {
             // * HEADER immagini (e logo FOOTER)
             images : {
-                news : "./img/meal-time-150x150.webp", //OK
-                logo : "./img/anime-logo-300x89.webp", //OK
-                banner : "./img/header-banner.webp", //OK
-                logoFooter: "./img/anime-whitelogo-300x89.png" //OK
+                news : "./img/meal-time-150x150.webp", 
+                logo : "./img/anime-logo-300x89.webp", 
+                banner : "./img/header-banner.webp", 
+                logoFooter: "./img/anime-whitelogo-300x89.png" 
             },
 
             // * HEADER icone
             icons : {
-                chevronLeft : "fa-solid fa-chevron-left", //OK
-                chevronRight : "fa-solid fa-chevron-right", //OK
-                facebook : "fa-brands fa-facebook-f", //OK
-                twitter : "fa-brands fa-twitter", //OK
-                instagram : "fa-brands fa-instagram", //OK
-                youtube : "fa-brands fa-youtube", //OK
-                burgerMenu : "fa-solid fa-bars", //OK
-                search : "fa-solid fa-magnifying-glass", //OK
+                chevronLeft : "fa-solid fa-chevron-left", 
+                chevronRight : "fa-solid fa-chevron-right", 
+                facebook : "fa-brands fa-facebook-f", 
+                twitter : "fa-brands fa-twitter", 
+                instagram : "fa-brands fa-instagram", 
+                youtube : "fa-brands fa-youtube", 
+                burgerMenu : "fa-solid fa-bars", 
+                search : "fa-solid fa-magnifying-glass", 
             },
 
             // * HEADER menù
             headerMenu : [
                 {   
-                    icon : "fa-solid fa-house-chimney", //OK
-                    section : "HOME", //OK
+                    icon : "fa-solid fa-house-chimney", 
+                    section : "HOME", 
                 },
                 {
-                    icon : "fa-solid fa-suitcase", //OK
-                    section : "LIFESTYLE", //OK
+                    icon : "fa-solid fa-suitcase", 
+                    section : "LIFESTYLE", 
                     chevron : "fa-solid fa-chevron-down"
                 },
                 {
-                    icon : "fa-solid fa-book-open-reader", //OK
-                    section : "STORIES", //OK
+                    icon : "fa-solid fa-book-open-reader", 
+                    section : "STORIES", 
                     chevron: "fa-solid fa-chevron-down"
 
                 },
                 {
-                    icon : "fa-solid fa-book", //OK
-                    section : "PAGES", //OK
+                    icon : "fa-solid fa-book", 
+                    section : "PAGES", 
                     chevron: "fa-solid fa-chevron-down"
                 },
                 {
-                    icon : "fa-solid fa-user-large", //OK
-                    section : "ABOUT US", //OK
+                    icon : "fa-solid fa-user-large", 
+                    section : "ABOUT US", 
                 },
             ],
 
@@ -77,14 +77,28 @@ createApp({
                     links : [ "Stories", "Community", "Blog", "Brand Assets" ],
                 },
             ],
+
+            webPageDesigner : "© Anime Teck, Theme by HasnainDEV",
+
+            newEmailAddress : [],
         }
     },
 
     methods : {
 
+        // * FOOTER input
         // raccolgo le email inserite
         addNewSubscriber() {
-            alert("added " + this.newSubscribers); //ok! :)
+            // alert("added " + this.newSubscribers); //ok! :)
+
+            if (this.newEmailAddress.includes("@") && this.newEmailAddress.includes(".")) {
+                console.log("L'indirizzo e-mail inserito è: " + this.newEmailAddress);
+            } else {
+                console.log("Ciò che hai inserito non è un indirizzo email!")
+            }
+
+            this.newEmailAddress = "";
         },
+
     }
 }).mount("#my-app");
